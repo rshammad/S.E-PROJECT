c56c33482e17e6e02b5e020c5f99370ce002104a
rshammad <63588814+rshammad@users.noreply.github.com>
2020-09-09 23:59:34 +0500

    update

---
+++ DoctorAppointmentSystem/about.php
@@ -0,0 +1,48 @@
+<?php include('header.php'); ?>
+
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+	<!-- this is for donor registraton -->
+	<div class="main_content" style="background-color:#fff;">
+		<h3 class="text-center" style="background-color:#272327;color: #fff;">About Us</h3>
+		<div class="col-md-12">
+<p >
+<div class="container"style="font-size: 18px ;padding: 40px">
+			Best Doctors’ innovative services include access to an unrivaled network of physicians who have been selected as the best in their field by other leading doctors. With every service offered, the goal remains the same: to help people in need get the right diagnosis and treatment, significantly improving health outcomes.</p>
+			
+</div>		
+<div style="margin-top: 3%;"><br></div>
+</div>
+
+
+          
+    </div>
+		
+	
+	
+	
+
+	
+ <?php include('footer.php'); ?>
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+	
+</body>
+</html>
---
+++ DoctorAppointmentSystem/index.php
@@ -0,0 +1,114 @@
+<?php include('header.php'); ?>
+
+	<!-- this is for welcome -->
+	
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!-- Team -->
+<section id="team" class="pb-5">
+    <div class="container">
+        <h5 class="section-title h1" >OUR DOCTORS</h5>
+        <div class="row">
+            <!-- Team member -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                    <div class="mainflip">
+                        <div class="frontside">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <p><img class=" img-fluid" src="img/doctor1.jpeg" alt="card image"></p>
+                                    <h4 class="card-title">Dr. Ruby Perrin </h4>
+                                    <p class="card-text">MDS - Periodontology and Oral Implantology, BDS.</p>
+                                    <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+            <!-- ./Team member -->
+            <!-- Team member -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                    <div class="mainflip">
+                        <div class="frontside">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <p><img class=" img-fluid" src="img/doctor2.jpeg" alt="card image"></p>
+                                    <h4 class="card-title">Dr. Darren Elder </h4>
+                                    <p class="card-text">BDS, MDS - Oral & Maxillofacial Surgery</p>
+                                    <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <!-- ./Team member -->
+            <!-- Team member -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                    <div class="mainflip">
+                        <div class="frontside">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <p><img class=" img-fluid" src="img/doctor3.jpeg" alt="card image"></p>
+                                    <h4 class="card-title">Dr. Deborah Angel </h4>
+                                    <p class="card-text">MBBS, MD - General Medicine, DNB - Cardiology	</p>
+                                    <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+            <!-- ./Team member -->
+            <!-- Team member -->
+            
+            <!-- ./Team member -->
+            <!-- Team member -->
+            
+            <!-- ./Team member -->
+            <!-- Team member -->
+
+            <!-- ./Team member -->
+
+        </div>
+    </div>
+</section>
+<!-- Team -->
+	<!-- nivo slider starts 
+	<div class="col-md-12 sliderImg">
+		<img src="img/doctor5.jpg" alt="">
+		<img src="img/doctor3.jpg" alt="">
+		<img src="img/doctor4.jpg" alt="">
+		<img src="img/hstu8.jpg"   alt="">
+		<img src="img/doctor3.jpg" alt="">
+		<img src="img/doctor4.jpg" alt="">
+	</div>
+	nivo slider ends -->
+
+	<!-- main Content -->
+	
+
+ 	<?php include('footer.php'); ?>
+
+
+	
+</div>	<!--  containerFluid Ends -->
+
+
+
+	<script src="js/jquery-1.9.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	 
+	
+</body>
+</html>
